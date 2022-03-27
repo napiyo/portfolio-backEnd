df2a459cb740555d6a6d1c176246c6de7b36c0ff
Narendra_Dewasi <radioactivenarendra@gmail.com>
2022-03-27 14:01:49 +0530

    setting cors

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const app = require('express')();
 const express = require('express');
 const messageRoutes = require('./routes/messageRoutes');
-const cors = require('cors');
 app.use(express.json())
-app.use(cors({origin:'*'}))
+
 app.use('/api/msg',messageRoutes);
 app.get('/', (req, res) => {
     res
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require('./database.js');
+const cors = require('cors');
 // dotenv.config({path:'./config.env'});
 connectDatabase();
+app.use(cors({}))
+app.use(cors({origin:'*'}))
 const PORT = process.env.PORT || 4101;
 app.listen(PORT);
