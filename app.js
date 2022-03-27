cd9799a2f3301d56728bc94b6a51858f6fab54f5
Narendra_Dewasi <radioactivenarendra@gmail.com>
2022-03-27 13:18:34 +0530

    setting cors

--- app.js
+++ app.js
@@ -3,24 +3,7 @@ const express = require('express');
 const messageRoutes = require('./routes/messageRoutes');
 const cors = require('cors');
 app.use(express.json())
-// app.use(cors({"origin": "*"}));
-// enable cors
-app.use(
-  cors({
-    origin: true,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
-app.options(
-  '*',
-  cors({
-    origin: true,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
-
+app.use(cors({credentials: true, origin: true, exposedHeaders: '*'}))
 app.use('/api/msg',messageRoutes);
 app.get('/', (req, res) => {
     res
