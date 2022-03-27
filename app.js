ca3f15baebd7fcaf9523819ef757804e873f3b4a
Narendra_Dewasi <radioactivenarendra@gmail.com>
2022-03-27 09:46:22 +0530

    deploying to heroku

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const cors = require('cors');
 app.use(express.json())
 app.use(cors())
 app.use('/api/msg',messageRoutes);
+app.get('/', (req, res) => {
+    res
+      .status(200)
+      .send('Hello server is running')
+      .end();
+  });
 module.exports=app;
