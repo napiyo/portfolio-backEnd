e2f336aafb4e5a155ac5672d8dbc34e6bdbd5144
Narendra_Dewasi <radioactivenarendra@gmail.com>
2022-03-27 14:49:54 +0530

    setting cors

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -6,7 +6,7 @@ exports.sendMessage = async(req,res,next)=>{
             return next("message and sender are required")
          }
         const msg = await messageSchema.create({message,sender});
-        res.set('Access-Control-Allow-Origin', '*');
+      
         res.status(201).json({
             sucess:true,
             msg,
