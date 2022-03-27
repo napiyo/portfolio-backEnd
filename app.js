52a31010ec428f581fc5f75112d492130c6413f8
Narendra_Dewasi <radioactivenarendra@gmail.com>
2022-03-27 09:06:47 +0530

    deploying

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const app = require('express')();
 const express = require('express');
 const messageRoutes = require('./routes/messageRoutes');
-
+const cors = require('cors');
 app.use(express.json())
-
+app.use(cors())
 app.use('/api/msg',messageRoutes);
 module.exports=app;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require('./database.js');
-dotenv.config({path:'./config.env'});
+// dotenv.config({path:'./config.env'});
 connectDatabase();
 const PORT = process.env.PORT || 4101;
-app.listen(PORT,()=>{
-    console.log(`server is running on http://localhost:${PORT}`);
-})
+app.listen(PORT);
